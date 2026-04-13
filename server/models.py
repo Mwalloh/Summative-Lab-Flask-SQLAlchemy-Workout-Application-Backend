9610ca3be0e8197b8b1f0975795219099fdde325
Jim <jimochieng64@gmail.com>
2026-04-13 22:12:57 +0300

    Implement Table Constraints and Model Validations

--- server/models.py
+++ server/models.py
@@ -21,6 +21,10 @@ class Exercise(db.Model):
     # Many-to-many with Workout through WorkoutExercise
     workout = db.relationship('Workout', secondary='workoutExercises')
     
+    __table_args__ = (
+        CheckConstraint("LENGTH(name) > 3", name='exercise_name_length_check'),
+    )
+    
     def __repr__(self):
         return f"Exercise(name={self.name}, category={self.category}, equipment_needed={self.equipment_needed})"
     
@@ -37,6 +41,16 @@ class Workout(db.Model):
     
     # Many-to-many with Exercise through WorkoutExercise
     exercise = db.relationship('Exercise', secondary='workoutExercises')
+    
+    __table_args__ = (
+        CheckConstraint("duration_minutes > 0", name='workout_duration'),
+    )
+    
+    @validates('duration_minutes')
+    def validate_duration_minutes(self, key, duration_minutes):
+        if not duration_minutes and duration_minutes < 0:
+            raise ValueError("Duration minutes cannot be empty and less than 0.")
+        return duration_minutes
 
     def __repr__(self):
         return f"Workout(date={self.date}, duration_minutes={self.duration_minutes}, notes={self.notes})"
@@ -53,6 +67,11 @@ class WorkoutExercises(db.Model):
     # Establish the r/ship to Workout and Exercise
     workout = db.relationship('Workout', back_populates='workoutExercise')
     exercise = db.relationship('Exercise', back_populates='workoutExercise')
+    
+    @validates('duration_seconds')
+    def validate_duration_seconds(self, key, duration_seconds):
+        if not duration_seconds and duration_seconds < 0:
+            raise ValueError("Duration seconds cannot be empty and less than 0.")
     
     def __repr__(self):
         return f"WorkoutExercise(workout_id={self.workout_id}, exercise_id={self.exercise_id}, reps={self.reps}, sets={self.reps}, duration_seconds={self.duration_seconds})"
